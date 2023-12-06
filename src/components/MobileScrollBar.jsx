037cb5d3ba260adc74497ad3e589fe76d0bede04
Diego Fischer <diego.fischer@nicepeopleatwork.com>
2023-12-06 16:55:14 +0000

    bigfix

--- src/components/MobileScrollBar.jsx
+++ src/components/MobileScrollBar.jsx
@@ -2,8 +2,10 @@
 
 import React, { useState, useEffect } from 'react'
 
-export function MobileScrollBar(width) {
-  const [isMobile, setIsMobile] = useState(false)
+export function MobileScrollBar({width}) {
+  const [isMobile, setIsMobile] = useState(true)
+
+  console.log(width)
 
   const checkIsMobile = () => {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera
@@ -16,9 +18,9 @@ export function MobileScrollBar(width) {
     setIsMobile(checkIsMobile())
   }, [])
 
-  if (isMobile) return (
+ if (isMobile) return (
     <>
-      <div className={`absolute bottom-0 left-0 z-10 h-3 w-[${width}px] bg-[#06b6d4]`}></div>
+      <div style={{width: `${width}px`}} className={`absolute bottom-0 left-0 z-10 h-3 bg-[#06b6d4]`}></div>
       <div className="absolute bottom-1 left-2 z-20 block h-1 w-[100px] rounded-full bg-gray-100 sm:hidden"></div>
     </>
   )
